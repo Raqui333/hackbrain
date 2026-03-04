5b372d18350e5f1e9f186078096d937b6afb3084
Raqui333 <klebersilva2025@gmail.com>
2026-03-03 23:18:37 -0300

    feat: enhance AssistantNode to include error handling and support for multiple tools

--- src/lib/agents/assistant/nodes/assistant.node.ts
+++ src/lib/agents/assistant/nodes/assistant.node.ts
@@ -2,22 +2,41 @@ import { AssistantState } from '@/lib/agents/assistant/state'
 import { SystemMessage } from '@langchain/core/messages'
 import { getLLMModel } from '@/src/lib/agents/llm/factory'
 import { GraphNode } from '@langchain/langgraph'
+
 import { createRetrievalTool } from '../../tools/retrieval.tool'
+import { createSearchTool } from '../../tools/search.tool'
 
 const RETRIEVAL_TOOL = createRetrievalTool()
+const SEARCH_TOOL = createSearchTool()
 
 export const AssistantNode: GraphNode<typeof AssistantState> = async (state, config) => {
-  console.log('[AssistantNode]: Running with model:', config.context?.model)
+  try {
+    console.log('[AssistantNode]: Running with model:', config.context?.model)
 
-  const llm = getLLMModel(config.context?.model)
-  const llmWithTools = llm.bindTools([RETRIEVAL_TOOL])
+    const llm = getLLMModel(config.context?.model)
+    const llmWithTools = llm.bindTools([RETRIEVAL_TOOL, SEARCH_TOOL])
 
-  const response = await llmWithTools.invoke([
-    new SystemMessage(config.context?.SYSTEM_PROMPT),
-    ...state.messages,
-  ])
+    const response = await llmWithTools.invoke([
+      new SystemMessage(config.context?.SYSTEM_PROMPT),
+      ...state.messages,
+    ])
 
-  return {
-    messages: [...state.messages, response],
+    return {
+      messages: [...state.messages, response],
+    }
+  } catch (error) {
+    console.error('[AssistantNode]: Error:', error)
+
+    let errorMessage = 'Ocorreu um erro, desculpa 😔'
+
+    if (error instanceof Error) {
+      if (error.message.includes('does not support tools')) {
+        errorMessage = 'O modelo que você selecionou não suporta ferramentas, tente outro modelo.'
+      }
+    }
+
+    return {
+      messages: [...state.messages, new SystemMessage(errorMessage)],
+    }
   }
 }
